4b0844cfd845e6c5cec72abedecca480334cdc3c
Divyanshu Pratap Singh <divyanshupratapsingh05@gmail.com>
2025-10-25 17:44:11 +0530

    Readme md updated

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,4 +3,31 @@ import express from "express";
 const app=express();
 app.use(express.json());
 
-app.listen(3003);
+
+app.post("/signup",async(req,res)=>{
+    const email=req.body.email;
+    const name=req.body.name;
+    const pasword=req.body.password;
+    const phot=""
+
+    res.json({
+        message:"Signed up"
+    })
+})
+
+app.post("/signin",async(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+
+    res.json({
+        message:"Signed in"
+    })
+})
+
+app.post("/chat",async(req,res)=>{
+    res.json({
+        mesaage:"chat"
+    })
+})
+
+app.listen(3001);
