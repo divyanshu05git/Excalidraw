a43bdf081b96843e3f1cbc4e5857955e5df7bff6
Divyanshu Pratap Singh <divyanshupratapsingh05@gmail.com>
2025-10-27 14:55:59 +0530

    db added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken"
+import "dotenv/config";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "./config";
 import { CreateUserSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/client"
+import bcrypt from "bcrypt";
 
 const app=express();
 app.use(express.json());
@@ -10,19 +13,31 @@ app.use(express.json());
 
 app.post("/signup",async(req,res)=>{
     const email=req.body.email;
+    const password=req.body.password;
     const name=req.body.name;
-    const pasword=req.body.password;
-    const photo=""
 
-    const data= CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message:"Incorrect inputs"
+    if(!email || !password || !name){
+        res.status(400).json({
+            message:"Missing required fields"
         })
     }
 
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prismaClient.user.create({
+        data:{
+            email:email,
+            password:hashedPassword,
+            name:name,
+            photo:"",
+
+        }
+    })
+
+
     res.json({
-        message:"Signed up"
+        message:"signed up"
     })
 })
 
--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,4 +1,9 @@
+import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
